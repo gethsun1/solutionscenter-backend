5f4d71d432e38ed76c96a23eee452d19a6c61bb7
Gethsun Misesi <gethsun09@gmail.com>
2024-12-07 18:25:03 +0300

    Update settings.py

--- solutions_center/settings.py
+++ solutions_center/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f=@bq%a1nor&xbgh#-aa4kmezg@zfn=5-(p^b1pu#$8n$d=q*t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'solutionscenter-backend-production.up.railway.app']
 
 
 # Application definition
