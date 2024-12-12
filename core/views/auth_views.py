5a73a09b7fee840cc86281a316ea0540a314460e
Gethsun Misesi <gethsun09@gmail.com>
2024-12-12 17:03:55 +0300

    Update auth_views.py

--- core/views/auth_views.py
+++ core/views/auth_views.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from django.core.mail import send_mail
 from django.conf import settings
 from rest_framework import status
+from django.contrib.auth import get_user_model, authenticate
 from ..serializers import RegisterSerializer, LoginSerializer
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
@@ -53,22 +53,27 @@ class LoginView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.validated_data['user']
+            username = serializer.validated_data['username']
+            password = serializer.validated_data['password']
+            user = authenticate(username=username, password=password)
 
-            # Generate JWT token
-            refresh = RefreshToken.for_user(user)
-            return Response({
-                "message": "Login successful!",
-                "user": {
-                    "id": user.id,
-                    "username": user.username,
-                    "email": user.email,
-                    "first_name": user.first_name,
-                    "last_name": user.last_name,
-                    "mobile_number": user.mobile_number,
-                },
-                "access_token": str(refresh.access_token),
-                "refresh_token": str(refresh),
-            }, status=status.HTTP_200_OK)
+            if user is not None:
+                # Generate JWT token
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    "message": "Login successful!",
+                    "user": {
+                        "id": user.id,
+                        "username": user.username,
+                        "email": user.email,
+                        "first_name": user.first_name,
+                        "last_name": user.last_name,
+                        "mobile_number": user.mobile_number,
+                    },
+                    "access_token": str(refresh.access_token),
+                    "refresh_token": str(refresh),
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
