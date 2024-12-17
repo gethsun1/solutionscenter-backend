d481b72a04a56bd7ed44fb8912dc691c90c8b6c5
Gethsun Misesi <gethsun09@gmail.com>
2024-12-17 20:19:33 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 # core/views.py
 
 from .views.solution_views import SolutionViewSet
-from .views.auth_views import RegisterView, LoginView
+from .views.auth_views import RegisterView, LoginView,UserView
 from .views.password_reset_views import PasswordResetView, PasswordResetConfirmView
