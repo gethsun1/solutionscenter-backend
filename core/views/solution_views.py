b71eae154374530b0720b1912e9c3205de73dca5
Gethsun Misesi <gethsun09@gmail.com>
2024-12-19 17:28:47 +0300

    Update solution_views.py

--- core/views/solution_views.py
+++ core/views/solution_views.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from ..models import Solution
 from ..serializers import SolutionSerializer
 
 class SolutionViewSet(viewsets.ModelViewSet):
     queryset = Solution.objects.all()
     serializer_class = SolutionSerializer
-    permission_classes = [IsAuthenticated]  # Ensure only authenticated users can access
+    permission_classes = [AllowAny]  # Ensure only authenticated users can access
 
     def perform_create(self, serializer):
         # Automatically assign the authenticated user to the solution
